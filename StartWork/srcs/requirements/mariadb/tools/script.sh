09c419cfe11ff22075ddf4b5c3076d6ea785aeed
zelabbas <zakariaelabbassi2002@gmail.com>
2024-09-28 23:34:18 +0100

    mariadb set up done

--- StartWork/srcs/requirements/mariadb/tools/script.sh
+++ StartWork/srcs/requirements/mariadb/tools/script.sh
@@ -10,27 +10,29 @@ echo "MYSQL_PASSWORD: $MYSQL_PASSWORD"
 # Starting the MariaDB service
 service mariadb start;
 
-echo "start CREATE THE DATA BASES"
+echo "Start creating the databases..."
 
 # Create the database if it doesn't exist
 mysql -u root -p"$MYSQL_ROOTPASSWORD" -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-echo "finish CREATE THE DATA BASES"
+
+echo "Finish creating the database."
+
 # Create the user if it doesn't exist
 mysql -u root -p$MYSQL_ROOTPASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 
 # Grant all privileges on the created database to the user
 mysql -u root -p$MYSQL_ROOTPASSWORD -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
 
-# Reload the grant tables to update its internal data structures with the current contents
-mysql -u root -p$MYSQL_ROOTPASSWORD -e "FLUSH PRIVILEGES;"
-
 # set the password for the root user of the MariaDB/MySQL server
 mysql -u root -p$MYSQL_ROOTPASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOTPASSWORD';"
+
+# Reload the grant tables to update its internal data structures with the current contents
+mysql -u root -p$MYSQL_ROOTPASSWORD -e "FLUSH PRIVILEGES;"
 
 # This shutdown happens to prevent MariaDB from continuing to run in the background unnecessarily during the setup phase.
 mysqladmin -u root -p$MYSQL_ROOTPASSWORD shutdown
 
-echo "done ✅"
+echo "Database setup completed successfully! ✅"
 # Start MariaDB daemon in the foreground to keep the container running
 exec mysqld_safe
 
