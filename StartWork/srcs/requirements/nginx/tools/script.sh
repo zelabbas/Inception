83e21c45209e96202d9d9f6804f0047ab0f7101a
zelabbas <zakariaelabbassi2002@gmail.com>
2024-10-01 21:14:11 +0100

    the script for nginx configuration file

--- StartWork/srcs/requirements/nginx/tools/script.sh
+++ StartWork/srcs/requirements/nginx/tools/script.sh
@@ -7,20 +7,22 @@ echo "server {
 
     # All files will be in this repo
     root /var/www/html;
-    index index.php index.html index.htm;
+    index index.php index.html index.htm index.nginx-debian.html;
 
     # SSL certificate paths
     ssl_certificate  /etc/nginx/ssl/certificate.crt;
     ssl_certificate_key /etc/nginx/ssl/private.key;
     ssl_protocols       TLSv1.2;
+" > /etc/nginx/sites-enabled/default
 
-    # PHP handling through FastCGI
+echo "    # PHP handling through FastCGI
     location ~ \.php$ {
-        try_files \$uri =404;  # Escape the dollar sign to ensure it's interpreted correctly
-        fastcgi_pass wordpress:9000; # Pass the request to the FastCGI server (PHP-FPM)
-        fastcgi_index index.php;     # Default file to serve for PHP requests
-        include fastcgi_params;      # Standard FastCGI parameters
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name; # Full path to the PHP script
-        fastcgi_param PATH_INFO \$fastcgi_path_info; # Additional path info (if any)
+		#fastcgi_pass wordpress:9000;
+        include snippets/fastcgi-php.conf;
     }
-}" > /etc/nginx/sites-enabled/default
+}" >> /etc/nginx/sites-enabled/default
+
+# Start Nginx in the foreground when the container starts
+exec nginx -g "daemon off;"
+
+echo "all good ✅"
