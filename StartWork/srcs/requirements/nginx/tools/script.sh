ebe9e8ae1ed9ba029fb87e0cb19575fac234ca65
zelabbas <zakriaelabbassi2002@gmail.com>
2024-10-05 17:32:44 +0100

    fix some problems && mandatory part is done

--- StartWork/srcs/requirements/nginx/tools/script.sh
+++ StartWork/srcs/requirements/nginx/tools/script.sh
@@ -3,7 +3,7 @@
 #SSL Configuration
 echo "server {
     listen 443 ssl;
-    server_name localhost;
+    server_name zelabbas.42.fr;
 
     # All files will be in this repo
     root /var/www/html;
@@ -12,7 +12,8 @@ echo "server {
     # SSL certificate paths
     ssl_certificate  $_CERT;
     ssl_certificate_key $_KEY;
-    ssl_protocols       TLSv1.2;
+    ssl_protocols       TLSv1.3;
+    include /etc/nginx/mime.types;
 " > /etc/nginx/sites-enabled/default
 
 echo '    # PHP handling through FastCGI
