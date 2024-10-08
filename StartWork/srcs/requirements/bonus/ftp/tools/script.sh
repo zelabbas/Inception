9dfae5cb4a47555ab9f36d302b91999c30a3a5e6
zelabbas <zakariaelabbassi2002@gmail.com>
2024-10-08 21:37:12 +0100

    add the service in the docker-compose

--- StartWork/srcs/requirements/bonus/ftp/tools/script.sh
+++ StartWork/srcs/requirements/bonus/ftp/tools/script.sh
@@ -1,58 +1,60 @@
 #!/bin/bash
 
-# start the vsftpd service in the background
+# Start the vsftpd (Very Secure FTP Daemon) service in the background
 service vsftpd start
 
-# create a user 
+# Create a new FTP user without a password prompt and without additional info
 adduser --gecos "" --disabled-password $FTP_USER
 
+# Set the password for the newly created FTP user
 echo "$FTP_USER:$FTP_PASSWORD_USER" | chpasswd
 
-# Create the ftp folder
+# Create an FTP directory for the user to store files
 mkdir -p /home/$FTP_USER/ftp
 
-# set its ownership
+# Set the ownership of the FTP directory to the new user
 chown -R $FTP_USER:$FTP_USER /home/$FTP_USER/ftp
 
-# Remove write permissions
+# Remove write permissions on the FTP directory for security reasons
 chmod a-w /home/$FTP_USER/ftp
 
-# Configure vsftpd
-# disables anonymous login to the FTP server
+# Configure vsftpd settings for secure FTP access
+
+# Disable anonymous login to enhance security
 echo "anonymous_enable=NO" >> /etc/vsftpd.conf
 
-# enables local user login
+# Enable local user login to allow authenticated users access
 echo "local_enable=YES" >> /etc/vsftpd.conf
 
-# enables write permissions for the FTP users.
+# Allow FTP users to write files in their home directories
 echo "write_enable=YES" >> /etc/vsftpd.conf
 
-# sets the chroot jail for local users to their home directory
+# Set the chroot jail for local users to their home directory for security
 echo "chroot_local_user=YES" >> /etc/vsftpd.conf
 
-# enables the use of the userlist_file option
+# Enable the userlist feature to manage allowed users
 echo "userlist_enable=YES" >> /etc/vsftpd.conf
 
-# sets the path to the userlist file
+# Specify the path for the userlist file
 echo "userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
 
-# allows write access for local within their chroot jail
+# Allow write access for users within their chroot jail
 echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
 
-# appends the value of the FTP_USER variable to the userlist file.
+# Add the FTP user to the userlist to allow them to connect
 echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
 
-# allow the users in  to connect in vsftpd 
+# Set userlist_deny to NO to allow users in the userlist to connect
 echo "userlist_deny=NO" >> /etc/vsftpd.conf
 
-# sets the minimum port for passive mode data connections
+# Define the minimum port for passive mode data connections
 echo "pasv_min_port=40011" >> /etc/vsftpd.conf
 
-# sets the maximum port for passive mode data connections
+# Define the maximum port for passive mode data connections
 echo "pasv_max_port=40020" >> /etc/vsftpd.conf
 
-# stop the service of vsftpd
+# Stop the vsftpd service to apply the new configuration
 service vsftpd stop
 
-# start the vsftpd in foreground 
+# Start vsftpd in the foreground to keep the container running
 /usr/sbin/vsftpd /etc/vsftpd.conf
